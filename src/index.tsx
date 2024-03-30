d97e77fa23f1a0743778450f1cea0c38959cdee8
Robert J. Kusznier <no-cheating@autistici.org>
2024-03-30 14:02:30 +0100

    Format project files with Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,10 @@ import { App } from "./App/App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
