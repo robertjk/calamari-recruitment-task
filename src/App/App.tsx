e7d6652997bbf692030eeac5d2f7d02ee8f7df31
Robert J. Kusznier <no-cheating@autistici.org>
2024-03-30 15:52:41 +0100

    Add basic markup for page header elements

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+
 import "./App.css";
 
+type Page = "all" | "favorites";
+
+const PAGE_TITLES: { [page in Page]: string } = {
+  all: "All specialists",
+  favorites: "My specialists",
+};
+
 function App() {
+  const [page, setPage] = useState<Page>("all");
+  const pageTitle = PAGE_TITLES[page];
+
   return (
-    <main className="App">
-      <h1>Calamari Recruitment Task</h1>
-    </main>
+    <div className="App">
+      <header>
+        <h1>{pageTitle}</h1>
+        <form>
+          <fieldset>
+            <label>
+              {PAGE_TITLES.all}
+              <input type="radio" name="page" value="all" />
+            </label>
+            <label>
+              {PAGE_TITLES.favorites}
+              <input type="radio" name="page" value="favorites" />
+            </label>
+          </fieldset>
+          <input type="search" name="searchText" />
+        </form>
+      </header>
+      <main>{/* TODO: Display specialists here */}</main>
+    </div>
   );
 }
 
