7a8a18ff6c173707ae6af8eee2b7917da5ce76f1
Robert J. Kusznier <no-cheating@autistici.org>
2024-04-01 13:15:28 +0200

    Add comment with GitHub Vitest issue

--- src/vitest-setup.ts
+++ src/vitest-setup.ts
@@ -2,4 +2,6 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
+// Fixing issue of Vitest integration with RTL:
+// https://github.com/vitest-dev/vitest/issues/1430
 afterEach(cleanup);
