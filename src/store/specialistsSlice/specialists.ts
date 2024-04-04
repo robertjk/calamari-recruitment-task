4898357d1bb941fd2ebe24caee63e9bd8d4a36bc
Robert J. Kusznier <no-cheating@autistici.org>
2024-04-04 19:14:11 +0200

    Populate store with specialists mock data

--- src/store/specialistsSlice/specialists.ts
+++ src/store/specialistsSlice/specialists.ts
@@ -1,8 +1,9 @@
 interface Specialist {
+  id: number;
   name: string;
   surname: string;
   profession: string;
-  photo?: string;
+  photoUrl?: string;
   rating: {
     sum: number;
     count: number;
@@ -11,10 +12,11 @@ interface Specialist {
 
 const specialists: Specialist[] = [
   {
+    id: 1,
     name: "John",
     surname: "Doe",
     profession: "serial killer",
-    photo:
+    photoUrl:
       "https://i.pinimg.com/originals/df/a2/e4/dfa2e40dac2012c6f5d9e5ed9a6db348.jpg",
     rating: {
       sum: 293,
@@ -22,10 +24,11 @@ const specialists: Specialist[] = [
     },
   },
   {
+    id: 2,
     name: "Kate",
     surname: "Bush",
     profession: "musician",
-    photo:
+    photoUrl:
       "https://bibliolore.org/wp-content/uploads/2018/07/kate-bush.jpg?w=500",
     rating: {
       sum: 1530,
@@ -33,10 +36,11 @@ const specialists: Specialist[] = [
     },
   },
   {
+    id: 3,
     name: "Pharoah",
     surname: "Sanders",
     profession: "musician",
-    photo:
+    photoUrl:
       "https://www.northseajazz.com/-/media/northseajazz/rotterdam/shows/2011/p/pharoah-sanders-quartet.jpg",
     rating: {
       sum: 88,
@@ -44,20 +48,22 @@ const specialists: Specialist[] = [
     },
   },
   {
+    id: 4,
     name: "James",
     surname: "McNulty",
     profession: "policeman",
-    photo: "https://s3.cine3.com/2016/07/1200-1200x675.jpg",
+    photoUrl: "https://s3.cine3.com/2016/07/1200-1200x675.jpg",
     rating: {
       sum: 5818,
       count: 1181,
     },
   },
   {
+    id: 5,
     name: "Joanna",
     surname: "Newsom",
     profession: "musician",
-    photo: "https://www.famousbirthdays.com/headshots/joanna-newsom-5.jpg",
+    photoUrl: "https://www.famousbirthdays.com/headshots/joanna-newsom-5.jpg",
     rating: {
       sum: 231,
       count: 51,
--- src/store/specialistsSlice/specialistsSlice.ts
+++ src/store/specialistsSlice/specialistsSlice.ts
@@ -4,8 +4,10 @@ import { Specialist, specialists } from "./specialists";
 
 type Page = "all" | "favorites";
 
+type SpecialistsRecord = Record<Specialist["id"], Specialist>;
+
 interface SpecialistsSliceState {
-  specialists: Specialist[];
+  specialists: SpecialistsRecord;
   page: Page;
   searchQuery: string;
 }
@@ -13,43 +15,63 @@ interface SpecialistsSliceState {
 const initialState: SpecialistsSliceState = {
   page: "all",
   searchQuery: "",
-  specialists,
+  specialists: specialists.reduce<SpecialistsRecord>((record, specialist) => {
+    record[specialist.id] = specialist;
+    return record;
+  }, {}),
 };
 
 const specialistsSlice = createSlice({
   name: "specialists",
   initialState,
   reducers: {
+    addSpecialists(state, action: PayloadAction<Specialist[]>) {
+      action.payload.forEach((specialist) => {
+        state.specialists[specialist.id] = specialist;
+      });
+    },
     setPage(state, action: PayloadAction<Page>) {
       state.page = action.payload;
     },
     setSearchQuery(state, action: PayloadAction<string>) {
       state.searchQuery = action.payload;
     },
-    setSpecialists(state, action: PayloadAction<Specialist[]>) {
-      state.specialists = action.payload;
-    },
   },
   selectors: {
     selectPage: (state) => state.page,
-    selectSpecialists: (state) => state.specialists,
+    selectSpecialistAverageRating: (state, id: Specialist["id"]) => {
+      const specialist = state.specialists[id];
+      return specialist.rating.sum / specialist.rating.count;
+    },
+    selectSpecialistFullName: (state, id: Specialist["id"]) => {
+      const specialist = state.specialists[id];
+      return `${specialist.name} ${specialist.surname}`;
+    },
+    selectSpecialists: (state) => Object.values(state.specialists),
     selectSearchQuery: (state) => state.searchQuery,
   },
 });
 
-const { setPage, setSearchQuery, setSpecialists } = specialistsSlice.actions;
+const { addSpecialists, setPage, setSearchQuery } = specialistsSlice.actions;
 
-const { selectPage, selectSpecialists, selectSearchQuery } =
-  specialistsSlice.selectors;
+const {
+  selectPage,
+  selectSpecialistAverageRating,
+  selectSpecialistFullName,
+  selectSpecialists,
+  selectSearchQuery,
+} = specialistsSlice.selectors;
 
 export {
+  addSpecialists,
   type Page,
   selectPage,
   selectSearchQuery,
+  selectSpecialistAverageRating,
+  selectSpecialistFullName,
   selectSpecialists,
   setPage,
   setSearchQuery,
-  setSpecialists,
   type Specialist,
   specialistsSlice,
   type SpecialistsSliceState,
