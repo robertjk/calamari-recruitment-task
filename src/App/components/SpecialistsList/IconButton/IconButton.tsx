d8b992bf103695a7580fe99288f08d4c2bf795a2
Robert J. Kusznier <no-cheating@autistici.org>
2024-04-25 20:59:25 +0200

    Refactor IconButton to use inline SVG

--- src/App/components/SpecialistsList/IconButton/IconButton.tsx
+++ src/App/components/SpecialistsList/IconButton/IconButton.tsx
@@ -2,6 +2,14 @@ import classNames from "classnames";
 import { type MouseEvent } from "react";
 
 import styles from "./IconButton.module.css";
+import BellIcon from "./icons/bell.svg?react";
+import CalendarIcon from "./icons/calendar.svg?react";
+import HeartClearIcon from "./icons/heart-clear.svg?react";
+import HeartFilledIcon from "./icons/heart-filled.svg?react";
+import MailIcon from "./icons/mail.svg?react";
+import StarClearIcon from "./icons/star-clear.svg?react";
+import StarFilledIcon from "./icons/star-filled.svg?react";
+import ThreeDotsIcon from "./icons/three-dots.svg?react";
 
 type Icon =
   | "bell"
@@ -12,6 +20,17 @@ type Icon =
   | "starClear"
   | "starFilled"
   | "threeDots";
+
+const ICONS = {
+  bell: BellIcon,
+  calendar: CalendarIcon,
+  heartClear: HeartClearIcon,
+  heartFilled: HeartFilledIcon,
+  mail: MailIcon,
+  starClear: StarClearIcon,
+  starFilled: StarFilledIcon,
+  threeDots: ThreeDotsIcon,
+};
 
 interface IconButtonProps {
   children: string;
@@ -28,15 +47,17 @@ function IconButton({
   onClick = () => undefined,
   title = "",
 }: IconButtonProps) {
-  const iconClassName = styles[`icon_${icon}`];
   const titleAttr = title || children;
+
+  const Icon = ICONS[icon];
 
   return (
     <button
-      className={classNames(styles.root, iconClassName, className)}
+      className={classNames(styles.root, className)}
       onClick={onClick}
       title={titleAttr}
     >
+      <Icon className={styles.icon} />
       <span className={styles.visuallyHiddenText}>{children}</span>
     </button>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "%components": path.resolve(__dirname, "src/app/components"),
